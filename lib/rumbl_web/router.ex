9a6aa902d899f0cee02cd27dcd0fa9a900184fff
Shantanu <bhardwaj.10@gmail.com>
2019-05-08 20:47:14 +0530

    Completed add new user

--- lib/rumbl_web/router.ex
+++ lib/rumbl_web/router.ex
@@ -17,7 +17,7 @@ defmodule RumblWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :index
-    resources "/users", UserController, [:index, :show, :new, :create]
+    resources "/users", UserController, only: [:index, :show, :new, :create]
   end
 
   # Other scopes may use custom stacks.
